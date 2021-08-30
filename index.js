7b79e7dbe197c84196591692d260786ee345a4e3
Young Jun Do <youngjundoe@gmail.com>
2021-08-30 17:29:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent="Young Jun Do is the champion";
+document.body.append(newHeader);
